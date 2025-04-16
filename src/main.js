a74fab89d8d1098a415d78f31d302116311a205d
XIAO <young.hsiao@outlook.com>
2025-04-16 22:25:13 +0800

    调整结构

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import {createRouter, createWebHashHistory} from 'vue-router'
-import Demo1 from '@/compoments/Demo1.vue'
-import Demo2 from '@/compoments/Demo2.vue'
-import User from '@/compoments/User.vue'
+import Demo1 from './components/Demo1.vue'
+import Demo2 from './components/Demo2.vue'
+import User from './components/User.vue'
+import Button from "primevue/button";
+import Aura from '@primeuix/themes/aura';
 
 const routes = [
     {path:'/demo1',component: Demo1},
@@ -17,5 +20,11 @@ const router = createRouter({
     history: createWebHashHistory(),
     routes: routes
 })
-
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    },
+    ripple: true
+});
+app.use(router).mount('#app');
